8465d073bf828adbb9c0e6aded40e239c25eb574
fartoot <fartoot1337@gmail.com>
2024-11-29 18:24:01 +0100

    add AuthProvider to main.jsx to share accessToken with all components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import GuestLayout from './layouts/GuestLayout.jsx'
-import Home from './pages/Home.jsx'
-import Post from './pages/Post.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GuestLayout from "./layouts/GuestLayout.jsx";
+import WrapLayout from "./layouts/WrapLayout.jsx";
+import Home from "./pages/Home.jsx";
+import Post from "./pages/Post.jsx";
+import Login from "./pages/Login.jsx";
+import "./index.css";
+import Register from "./pages/Register.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<GuestLayout />}>
-          <Route index element={<Home />} />
-          <Route path="post" element={<Post />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WrapLayout />}>
+            <Route path="/" element={<GuestLayout />}>
+              <Route index element={<Home />} />
+              <Route path="post" element={<Post />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  </AuthProvider>
+);
