b1a462c41248cf9520308815e7d0df57f68c15d5
fartoot <fartoot1337@gmail.com>
2024-12-03 19:20:48 +0100

    fix: remove old manual migration way for all models with new one

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 
 from app.config import settings
 from app.database import Base
+from app.models import Post, User, Vote
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -20,8 +21,8 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-target_metadata = Base.metadata
-# target_metadata = None
+# target_metadata = mymodel.Base.metadata
+target_metadata = Base.metadata 
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
