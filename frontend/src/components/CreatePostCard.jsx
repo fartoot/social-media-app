bb8b53f3e390feb4c3c6d3dc9f18de760d28fa27
fartoot <fartoot1337@gmail.com>
2024-12-04 00:50:36 +0100

    frontEnd | add : create component for publishing posts

---
+++ frontend/src/components/CreatePostCard.jsx
@@ -0,0 +1,63 @@
+import { AuthContext } from "../context/AuthContext";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useContext } from "react";
+
+const formSchema = z.object({
+  content: z.string().min(1).max(200),
+});
+
+function CreatePostCard() {
+  const { accessToken } = useContext(AuthContext)
+  const {
+     register,
+     handleSubmit,
+     formState: { errors },
+   } = useForm({
+     resolver: zodResolver(formSchema),
+   });
+   const onSubmit = async (data) => {
+     const url = 'http://127.0.0.1:8000/posts';
+      const options = {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'content-type': 'application/json'
+        },
+        body : JSON.stringify({
+          content: data.content
+        })
+      };
+ 
+     try {
+       const response = await fetch(url, options);
+       const data = await response.json();
+     } catch (error) {
+       console.error(error);
+     }
+   }; 
+  
+  return (
+    <>
+      <form 
+        onSubmit={handleSubmit(onSubmit)}
+      >
+      <div className="w-96 bg-gray-100 rounded-3xl p-6 space-y-3 mx-auto">
+        <div className="flex justify-between items-center">
+          <h2 className="text-left w-full text-gray-800 ms-2">Create Post</h2>
+          <button className="bg-gray-300 py-2 px-3 rounded-full text-sm">Publish</button>
+        </div>
+        <textarea {...register("content")} rows={4} placeholder="Something..." className="bg-gray-200 text-gray-800 rounded-2xl w-full px-4 py-2"></textarea>
+        {errors.content && (
+          <div className="text-red-500 text-xs italic">
+                    {errors.content.message}
+          </div>
+        )}
+      </div>
+      </form>
+    </>
+  )
+}
+
+export default CreatePostCard 
