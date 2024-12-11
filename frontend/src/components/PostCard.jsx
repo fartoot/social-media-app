cac5a1d65581ad6e0e15dc9882d11296abf4a942
fartoot <fartoot1337@gmail.com>
2024-12-11 23:33:11 +0100

    implement PostCard component

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,17 +1,17 @@
-function PostCard() {
+function PostCard({firstName, lastName, username,  createdAt, content}) {
   return (
     <>
-      <div className="bg-gray-50 rounded-md p-7 my-6">
+      <div className="bg-gray-100 rounded-md p-7 w-full md:min-w-96">
         <div className="flex">
-          <div className="bg-gray-200 w-12 h-12 rounded-full me-5"></div>
+          <div className="bg-gray-300 w-12 h-12 rounded-full me-5"></div>
           <div>
-            <p>james taylor</p>
-            <p className="text-sm">@jamestaylor</p>
+            <p>{firstName} {lastName}</p>
+            <p className="text-sm">@{ username }</p>
           </div>
-          <div className="text-gray-600 text-sm leading-6">&nbsp;&nbsp;1h ago</div>
+          <div className="text-gray-600 text-sm leading-6">&nbsp;&nbsp;{createdAt}</div>
         </div>
         <div className="my-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla recusandae nam facere pariatur, sint ratione excepturi doloribus quaerat ea reprehenderit. Aperiam deleniti inventore recusandae. Obcaecati amet distinctio nemo tempora necessitatibus.
+          {content}
         </div>
       </div>
     </>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,32 @@
-import PostCard from "../components/PostCard" 
+import PostCard from "../components/PostCard"
+import { useState, useEffect, useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
+import moment from"moment";
 
 function Home() {
+  const [posts, setPosts] = useState([])
+  const { accessToken } = useContext(AuthContext)
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      };
+      const response = await fetch("http://127.0.0.1:8000/posts/", options)
+      const postsData = await response.json()
+      setPosts(postsData)
+      console.log(postsData)
+    }
+    fetchPosts()
+  }, [accessToken])
 
   return (
     <>
-      <PostCard />
+      {posts.map((data) => <PostCard key={data.id} firstName={data.owner.first_name} lastName={data.owner.last_name} username={data.owner.username} createdAt={moment(data.owner.created_at).fromNow()} content={data.content}  /> )}
     </>
   )
 }
 
-export default Home 
+export default Home
