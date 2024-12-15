333778b81c81578e3ba8f28f62136d32c33a8ce4
fartoot <fartoot1337@gmail.com>
2024-12-15 23:37:29 +0100

    add hyperlinks to postcard

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,12 +1,15 @@
-function PostCard({firstName, lastName, username,  createdAt, content}) {
+import { Link } from "react-router-dom";
+function PostCard({slug, firstName, lastName, username,  createdAt, content}) {
   return (
     <>
       <div className="bg-gray-100 rounded-md p-7 w-full md:min-w-96">
         <div className="flex">
           <div className="bg-gray-300 w-12 h-12 rounded-full me-5"></div>
           <div>
-            <p>{firstName} {lastName}</p>
-            <p className="text-sm">@{ username }</p>
+            <Link to={`/profile/${slug}`}>
+              <p>{firstName} {lastName}</p>
+              <p className="text-sm block">@{ username }</p>
+            </Link>
           </div>
           <div className="text-gray-600 text-sm leading-6">&nbsp;&nbsp;{createdAt}</div>
         </div>
